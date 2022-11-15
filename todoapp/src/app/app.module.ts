2f6e2fb0852b641b902aa5cc92dc647faa38613a
Tyler Smith <dts5004@gmail.com>
2022-11-15 16:27:30 -0500

    Reasonable card example

--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +19,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
