9dbe0d8e77353d19aec1f393931df3946a9895e3
Tyler Smith <dts5004@gmail.com>
2022-11-15 15:34:58 -0500

    Add example toolbar

--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {MatButtonModule} from '@angular/material/button'
-import {DragDropModule} from '@angular/cdk/drag-drop'
+import { MatButtonModule } from '@angular/material/button'
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +15,9 @@ import {DragDropModule} from '@angular/cdk/drag-drop'
     BrowserModule,
     DragDropModule,
     MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
