b5f0c0a409aa79b99225c601ef3ac16d310de6aa
Tyler Smith <dts5004@gmail.com>
2022-11-15 14:04:48 -0500

    Make working drag and drop todo example without cards

--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatButtonModule} from '@angular/material/button'
+import {DragDropModule} from '@angular/cdk/drag-drop'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DragDropModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
